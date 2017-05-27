ca76838b43e0007fb6efc27fcbba1559110a8e22
Jackson Ng <jacksonng@outlook.com>
2017-05-27 16:17:31 +0800

    Initial Release

---
+++ C7/C7/www/scripts/common.js
@@ -0,0 +1,30 @@
+﻿function wages(hoursWorked, hourlyRate) {
+    var totalsalary = hoursWorked * hourlyRate;
+    return totalsalary;
+}
+
+function home() {
+    window.location = "index.html";
+}
+
+function hello() {
+    window.location = "hello.html";
+}
+
+function goodbye() {
+    window.location = "goodbye.html";
+}
+
+function getRadioValue(groupName) {
+    var _result;
+    try {
+        var o_radio_group = document.getElementsByName(groupName);
+        for (var a = 0; a < o_radio_group.length; a++) {
+            if (o_radio_group[a].checked) {
+                _result = o_radio_group[a].value;
+                break;
+            }
+        }
+    } catch (e) { }
+    return _result;
+}
---
+++ C7/C7/www/scripts/index.js
@@ -0,0 +1,27 @@
+﻿// For an introduction to the Blank template, see the following documentation:
+// http://go.microsoft.com/fwlink/?LinkID=397704
+// To debug code on page load in cordova-simulate or on Android devices/emulators: launch your app, set breakpoints, 
+// and then run "window.location.reload()" in the JavaScript Console.
+(function () {
+
+    function checkCost() {
+        var toyPrice, budget, conduct, testScore;
+
+        testScore = 100;
+        toyPrice = 15.5;
+        budget = 16;
+        conduct = "naughty";
+
+        if ((testScore === 100) || ((toyPrice <= budget) && (conduct === "good"))) {
+            alert("Allowed to buy");
+        }
+        alert("Toy Price is $" + toyPrice + " and budget is $" + budget);
+    }
+
+    checkCost();
+
+
+
+
+
+} )();
